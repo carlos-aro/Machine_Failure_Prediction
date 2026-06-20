6e647667c0770b8c7b704df7474b59eb45bb574f
carlos-aro <64565457+carlos-aro@users.noreply.github.com>
2026-06-20 23:46:27 +0300

    Update hosting.py

--- week_2_mls/hosting/hosting.py
+++ week_2_mls/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="week_2_mls/deployment",     # the local folder containing your files
-    repo_id="praneeth232/Machine-Failure-Prediction",          # the target repo
+    repo_id="carlosaro/Machine-Failure-Prediction",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
