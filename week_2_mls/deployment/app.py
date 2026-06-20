33e0d4e81f43111feef5fd10208a7cc3a66b59a5
carlos-aro <64565457+carlos-aro@users.noreply.github.com>
2026-06-20 23:45:35 +0300

    Update app.py

--- week_2_mls/deployment/app.py
+++ week_2_mls/deployment/app.py
@@ -4,7 +4,7 @@ from huggingface_hub import hf_hub_download
 import joblib
 
 # Download and load the model
-model_path = hf_hub_download(repo_id="praneeth232/machine_failure_model", filename="best_machine_failure_model_v1.joblib")
+model_path = hf_hub_download(repo_id="carlosaro/machine_failure_model", filename="best_machine_failure_model.joblib")
 model = joblib.load(model_path)
 
 # Streamlit UI for Machine Failure Prediction
